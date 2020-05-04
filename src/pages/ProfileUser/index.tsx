0a7607768566ad2e3f58c1b54d51b15e858f4c49
Diego Souza <dee-souza@hotmail.com>
2020-05-04 15:18:54 -0300

    :art: Create page profile

--- src/pages/ProfileUser/index.tsx
+++ src/pages/ProfileUser/index.tsx
@@ -1,12 +1,69 @@
 import React from 'react';
+import { View, KeyboardAvoidingView, ScrollView, Platform } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useAuth } from '../../hooks/auth';
 
-import { Container, ContainerText } from './styles';
+import {
+  Container,
+  Title,
+  BackToSignIn,
+  BackToSignInText,
+  BackToGoBack,
+  BackToGoBackText,
+  ImageLogo,
+  BoxProfile,
+  BoxProfileText,
+  IconInput,
+} from './styles';
 
 const ProfileUser: React.FC = () => {
+  const { signOut, user } = useAuth();
+  const navigation = useNavigation();
+
   return (
-    <Container>
-      <ContainerText>Profile</ContainerText>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <ImageLogo
+              source={{
+                uri: `https://api.adorable.io/avatars/285/${user.userData.name}`,
+              }}
+            />
+
+            <View>
+              <Title>Informações da Conta</Title>
+            </View>
+
+            <BoxProfile>
+              <IconInput name="user" size={20} color="#aaaaaa" />
+              <BoxProfileText>{user.userData.name}</BoxProfileText>
+            </BoxProfile>
+
+            <BoxProfile>
+              <IconInput name="mail" size={20} color="#aaaaaa" />
+              <BoxProfileText>{user.userData.email}</BoxProfileText>
+            </BoxProfile>
+
+            <BackToGoBack onPress={() => navigation.goBack()}>
+              <IconInput name="arrow-left" size={20} color="#33495d" />
+              <BackToGoBackText>VOLTAR</BackToGoBackText>
+            </BackToGoBack>
+
+            <BackToSignIn onPress={signOut}>
+              <BackToSignInText>SAIR DO SAFERCE</BackToSignInText>
+            </BackToSignIn>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 
--- src/pages/ProfileUser/styles.ts
+++ src/pages/ProfileUser/styles.ts
@@ -1,9 +1,80 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
+import Theme from '../../theme';
 
 export const Container = styled.View`
   flex: 1;
+  align-items: center;
   justify-content: center;
+  padding: 0px 30px ${Platform.OS === 'android' ? 150 : 40}px;
+`;
+
+export const ImageLogo = styled.Image`
+  height: 120px;
+  width: 120px;
+  margin-top: 50px;
+  border-radius: 60px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #666666;
+  font-family: 'RobotoSlab-Regular';
+  margin: 44px 0px 24px;
+`;
+
+export const BackToSignIn = styled.TouchableOpacity`
+  align-items: center;
+  color: #ff9000;
+  flex-direction: row;
+  justify-content: center;
+  padding: 16px 0px;
+  margin-top: 20px;
+`;
+
+export const BackToSignInText = styled.Text`
+  color: ${Theme.color.pink};
+  font-size: 14px;
+  font-family: 'RobotoSlab-Bold';
+  margin-left: 16px;
+`;
+
+export const BackToGoBack = styled.TouchableOpacity`
+  align-items: center;
+  color: #ff9000;
+  flex-direction: row;
+  justify-content: center;
+  padding: 16px 0px;
+  margin-top: 20px;
+`;
+
+export const BackToGoBackText = styled.Text`
+  color: ${Theme.color.secondary};
+  font-size: 14px;
+  font-family: 'RobotoSlab-Bold';
+  margin-left: 0px;
+`;
+
+export const BoxProfile = styled.View`
+  width: 100%;
+  height: 60px;
+  padding: 0px 16px;
+  background: #ffffff;
+  border-radius: 10px;
+  margin-bottom: 12px;
+  border-width: 1px;
+  border-color: #cccccc;
+  flex-direction: row;
   align-items: center;
 `;
 
-export const ContainerText = styled.Text``;
+export const BoxProfileText = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+`;
+
+export const IconInput = styled(FeatherIcon)`
+  margin-right: 16px;
+`;
